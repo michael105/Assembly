66185f600bbd13cbfec373a9049661afc9f7da40
Michael Myer <misc.myer@zoho.com>
2021-11-19 04:54:12 +0000

    add comment

--- bzero.amd64.s
+++ bzero.amd64.s
@@ -1,3 +1,10 @@
+/* my experiments showed, at least here,
+ there are speed optimizations possible.
+ However, rep stosb is fast, and I could measure
+ a real performance gain by e.g. vectorization
+ only for memory areas beginning with, something about 4kB.
+ Then it's on the other hand quite more sensitiv to use sse instructions */
+
 // bzero( void *s, unsigned int n ) 
 bzero:
 	xor %eax,%eax
